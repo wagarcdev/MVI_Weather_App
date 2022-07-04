e3e74b59350c67c613c7bc96035de88ce244aa22
Wagner Arcieri <84655093+WagnerArcieri@users.noreply.github.com>
2022-07-03 21:50:12 -0300

    Initial commit - configured dependencies and added resources

---
+++ app/src/main/java/com/arcieri/wagner/mviweatherapp/ui/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.arcieri.wagner.mviweatherapp.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun MVIWeatherAppTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}

644e314af228b0b5514090e7b52a8ee889acf4b2
Wagner Arcieri <84655093+WagnerArcieri@users.noreply.github.com>
2022-07-03 22:45:41 -0300

    created WeatherData and weatherInfo data classes and a WeatherMapper

---
+++ app/src/main/java/com/arcieri/wagner/mviweatherapp/domain/weather/WeatherInfo.kt
@@ -0,0 +1,7 @@
+package com.arcieri.wagner.mviweatherapp.domain.weather
+
+data class WeatherInfo(
+    val weatherDataPerDay: Map<Int, List<WeatherData>>,
+    val currentWeatherData: WeatherData?
+) {
+}
